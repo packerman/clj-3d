10c8373c5a0a2e472e5f9137606b5e4736148da4
Piotr Ackermann <pioacker@gmail.com>
2017-01-22 12:04:41 +0100

    2d figures
    - polygon
    -circle refactor

--- src/clj_3d/example/simple_scene.clj
+++ src/clj_3d/example/simple_scene.clj
@@ -10,31 +10,32 @@
    [x-min y-max] [x-max y-min] [x-max y-max]])
 
 (defn circle-vertex-array
-  ([center-x center-y radius n]
-   (letfn [(alpha [k]
-             (/ (* 2 Math/PI k) n))
-           (dx [alpha]
-             (* radius (Math/cos alpha)))
-           (dy [alpha]
-             (* radius (Math/sin alpha)))]
-     (->>
-       (for [k (range n)]
-         (list
-           [center-x center-y]
-           [(+ center-x (dx (alpha k))) (+ center-y (dy (alpha k)))]
-           [(+ center-x (dx (alpha (inc k)))) (+ center-y (dy (alpha (inc k))))]))
-       (mapcat identity))))
-  ([center-x center-y radius] (circle-vertex-array center-x center-y radius 100)))
+  ([[center-x center-y :as center] radius n]
+   (->>
+     (iterate #(+ (/ (* 2 Math/PI) n) %) 0)
+     (take (inc n))
+     (map #(vector (+ center-x (* radius (Math/cos %)))
+                   (+ center-y (* radius (Math/sin %)))))
+     (partition 2 1)
+     (map (fn [[p1 p2]] (list center p1 p2)))
+     (mapcat identity)))
+  ([center radius] (circle-vertex-array center radius 96)))
+
+(defn polygon-vertex-array [points]
+  (let [[first-point & rest-points] points]
+    (->>
+      (for [[p1 p2] (partition 2 1 rest-points)]
+        (list first-point p1 p2))
+      (mapcat identity))))
 
 (def scene [{:vertex-array [[-0.7 0.9] [-0.9 0.1] [-0.1 0.1]]
              :color        color/orange}
             {:vertex-array (rectangle-vertex-array 0.1 0.1 0.9 0.9)
              :color        color/lime}
-            {:vertex-array (circle-vertex-array -0.5 -0.5 0.4)
+            {:vertex-array (circle-vertex-array [-0.5 -0.5] 0.4)
              :color color/red}
-            ;{:vertex-array [] ; polygon
-            ; :color color/yellow }
-            ])
+            {:vertex-array (polygon-vertex-array [[0.1 -0.4] [0.1 -0.9] [0.9 -0.9] [0.9 -0.4] [0.5 -0.1]])
+             :color color/yellow}])
 
 (defn make [gl]
   (common/make-application-for-scene gl scene))
